66222a0ae83cf4bfac9cbe53936751dc328660be
ShanteTorres <kennedy2110@gmail.com>
2021-11-07 22:07:58 -0500

    Completed assignment

--- test/index-test.js
+++ test/index-test.js
@@ -1,7 +1,7 @@
 const { name, height, message } = require("../index.js");
 
 describe("what-is-a-test", () => {
-  describe("Name", () => {
+  describe("Susan", () => {
     it('returns "Susan"', () => {
       expect(name).toEqual("Susan");
     });
